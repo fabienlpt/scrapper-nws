ca909abb23fb11df2a302a4ca74a1ec5643c39fc
Fabien Lapert <lapertfabien@gmail.com>
2023-05-08 21:31:43 +0200

    update sql request

--- js/storeSQL.js
+++ js/storeSQL.js
@@ -1,82 +1,93 @@
-import mysql from 'mysql2/promise';
-import fs from 'fs';
+import mysql from "mysql2/promise";
+import fs from "fs";
 
 (async () => {
-    const connection = await mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "restaurant",
-    });
+  const dbConnection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "restaurant",
+  });
 
-    await connection.execute(`CREATE TABLE IF NOT EXISTS recettes (
+  // create recettes table to store recipes
+  await dbConnection.execute(`CREATE TABLE IF NOT EXISTS recettes (
       id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
       titre VARCHAR(255) NOT NULL,
       image VARCHAR(255),
       instructions JSON
     )`);
-  
-    await connection.execute(`CREATE TABLE IF NOT EXISTS ingredients (
+
+  // create ingredients table to store ingredients
+  await dbConnection.execute(`CREATE TABLE IF NOT EXISTS ingredients (
       id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
       nom VARCHAR(255) NOT NULL
     )`);
-  
-    await connection.execute(`CREATE TABLE IF NOT EXISTS ingredients_recettes (
+
+  // create ingredients_recettes table to store the relationship between recipes and ingredients
+  await dbConnection.execute(`CREATE TABLE IF NOT EXISTS ingredients_recettes (
       id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
       recette_id INT(11) NOT NULL,
       ingredient_id INT(11) NOT NULL,
       FOREIGN KEY (recette_id) REFERENCES recettes(id),
       FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
     )`);
-  
-    const jsonFilePathRecipes = "./json/recettes.json";
-  
-    try {
-      const data = await fs.promises.readFile(jsonFilePathRecipes, "utf8");
-      const recipes = JSON.parse(data);
-      for (const recipe of recipes) {
-        const [existingRecipe] = await connection.execute(
-          "SELECT id FROM recettes WHERE titre = ?",
-          [recipe.titre]
+
+  const recipesJsonFilePath = "./json/recettes.json";
+
+  try {
+    const data = await fs.promises.readFile(recipesJsonFilePath, "utf8");
+    const recipes = JSON.parse(data);
+
+    for (const recipe of recipes) {
+      // Vériier si la recette existe déjà dans la base de données
+      const [existingRecipe] = await dbConnection.execute(
+        "SELECT id FROM recettes WHERE titre = ?",
+        [recipe.titre]
+      );
+
+      let recetteId;
+      if (existingRecipe.length === 0) {
+        // Si la recette n'existe pas, on l'insère dans la base de données
+        const [result] = await dbConnection.execute(
+          "INSERT INTO recettes (titre, image, instructions) VALUES (?, ?, ?)",
+          [recipe.titre, recipe.image, JSON.stringify(recipe.instructions)]
         );
-        let recetteId;
-        if (existingRecipe.length === 0) {
-          const [result] = await connection.execute(
-            "INSERT INTO recettes (titre, image, instructions) VALUES (?, ?, ?)",
-            [recipe.titre, recipe.image, JSON.stringify(recipe.instructions)]
-          );
-          recetteId = result.insertId;
-        } else {
-          recetteId = existingRecipe[0].id;
-        }
-        for (const ingredient of recipe.ingredients) {
-          const [existingIngredient] = await connection.execute(
-            "SELECT id FROM ingredients WHERE nom = ?",
+        recetteId = result.insertId;
+      } else {
+        // Si la recette existe déjà, on récupère son id
+        recetteId = existingRecipe[0].id;
+      }
+
+      for (const ingredient of recipe.ingredients) {
+        // Vérifier si l'ingrédient existe déjà dans la base de données
+        const [existingIngredient] = await dbConnection.execute(
+          "SELECT id FROM ingredients WHERE nom = ?",
+          [ingredient.name]
+        );
+
+        let ingredientId;
+        if (existingIngredient.length === 0) {
+          // Si l'ingrédient n'existe pas, on l'insère dans la base de données
+          const [ingredientResult] = await dbConnection.execute(
+            "INSERT INTO ingredients (nom) VALUES (?)",
             [ingredient.name]
           );
-          let ingredientId;
-          if (existingIngredient.length === 0) {
-            const [ingredientResult] = await connection.execute(
-              "INSERT INTO ingredients (nom) VALUES (?)",
-              [ingredient.name]
-            );
-            ingredientId = ingredientResult.insertId;
-          } else {
-            ingredientId = existingIngredient[0].id;
-          }
-  
-          await connection.execute(
-            "INSERT INTO ingredients_recettes (recette_id, ingredient_id) VALUES (?, ?)",
-            [recetteId, ingredientId]
-          );
+          ingredientId = ingredientResult.insertId;
+        } else {
+          // Si l'ingrédient existe déjà, on récupère son id
+          ingredientId = existingIngredient[0].id;
         }
+
+        await dbConnection.execute(
+          "INSERT INTO ingredients_recettes (recette_id, ingredient_id) VALUES (?, ?)",
+          [recetteId, ingredientId]
+        );
       }
-      console.log(
-        `Les recettes et les ingrédients ont été insérés avec succès dans la base de données!`
-      );
-    } catch (err) {
-      console.error(err);
     }
-  
-    await connection.end();
-  })();
+		console.log("Les données sont bien rentrées dans la base de données");
+  } catch (err) {
+    console.error(err);
+  }
+
+  await dbConnection.end();
+})();
