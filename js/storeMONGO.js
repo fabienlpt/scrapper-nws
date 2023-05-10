9e3bab844cdccb434ad0232755cabb0e5bb52f50
fabienlpt <lapertfabien@gmail.com>
2023-05-10 20:10:09 +0200

    update store mongo

--- js/storeMONGO.js
+++ js/storeMONGO.js
@@ -5,7 +5,6 @@ import {
   recipesJsonFilePath,
 } from "./config.js";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(mongoUrl, {
   serverApi: {
     version: ServerApiVersion.v1,
